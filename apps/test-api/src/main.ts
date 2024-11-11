9479a82742792892b21ea4144ea11ae192c3d67b
Thomas Chuah <thomaschuah@users-MacBook-Pro.local>
2024-11-11 14:33:38 -0800

    Initial commit

---
+++ apps/test-api/src/main.ts
@@ -0,0 +1,38 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import express from 'express';
+import * as path from 'path';
+
+const app = express();
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/api', (req, res) => {
+  res.send({ message: 'Welcome to test-api!' });
+});
+
+app.get("/data", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      todo: "Wash the dishes"
+    }, 
+    {
+      id: 2,
+      todo: "Walk the dog"
+    },
+    {
+      id: 3,
+      todo: "Vacuum the floors"
+    }
+  ])
+})
+
+const port = process.env.PORT || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
